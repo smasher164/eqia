8176ebb78bbbf813ff6607b0ca3316d2dc1bbb49
smasher164 <aindurti@gmail.com>
2020-06-08 02:17:42 -0400

    support civic information api

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/smasher164/eqia
 
 go 1.14
+
+require (
+	cloud.google.com/go v0.56.0
+	google.golang.org/api v0.26.0
+	google.golang.org/genproto v0.0.0-20200331122359-1ee6d9798940
+)
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"google.golang.org/api/civicinfo/v2"
+	"google.golang.org/api/option"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 )
 
 func main() {
@@ -11,7 +18,31 @@ func main() {
 	if port == ":" {
 		port += "8080"
 	}
+	civicKey := os.Getenv("CIVIC_KEY")
+	ctx := context.Background()
+	if civicKey == "" {
+		client, err := secretmanager.NewClient(ctx)
+		if err != nil {
+			log.Fatalf("failed to create secretmanager client: %v", err)
+		}
+		req := &secretmanagerpb.AccessSecretVersionRequest{
+			Name: "projects/end-qualified-immunity/secrets/civic-key/versions/latest",
+		}
+		result, err := client.AccessSecretVersion(ctx, req)
+		if err != nil {
+			log.Fatalf("failed to access secret version: %v", err)
+		}
+		civicKey = string(result.Payload.Data)
+	}
+	civicinfoService, err := civicinfo.NewService(ctx, option.WithAPIKey(civicKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := civicinfoService.Representatives.RepresentativeInfoByDivision("ocd-division/country:us", nil).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.ListenAndServe(port, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World!")
+		fmt.Fprintln(w, data.Divisions)
 	}))
 }
